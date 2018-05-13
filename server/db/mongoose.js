4e45f1679aca89694a9087748caaa598d1df2151
Temitayo Oyelowo <temitayooyelowo@gmail.com>
2018-05-13 11:13:14 -0400

    Updated db url and node version

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,13 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://admin:admin@1@ds221990.mlab.com:21990/node-todo-api'
-};
-
-mongoose.connect(db.localhost || db.mlab);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose: mongoose
